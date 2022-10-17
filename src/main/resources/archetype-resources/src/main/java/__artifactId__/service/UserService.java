d31e3d9d16d38689a83af8de9ae2a1d35753aee7
israiloff <stardustindark@gmail.com>
2022-10-17 16:27:35 +0500

    fixed: user service's logic;
    Signed-off-by: israiloff <stardustindark@gmail.com>

--- src/main/resources/archetype-resources/src/main/java/__artifactId__/service/UserService.java
+++ src/main/resources/archetype-resources/src/main/java/__artifactId__/service/UserService.java
@@ -3,9 +3,11 @@
 #set($symbol_escape='\' )
 package ${package}.${artifactId}.service;
 
+import reactor.core.publisher.Mono;
 import org.springframework.security.core.Authentication;
 import ${package}.${artifactId}.model.UserCredentials;
 
 public interface UserService {
-    UserCredentials getUserCredentials(Authentication authentication);
+
+    Mono<UserCredentials> getCredentials();
 }
--- src/main/resources/archetype-resources/src/main/java/__artifactId__/service/impl/UserServiceImpl.java
+++ src/main/resources/archetype-resources/src/main/java/__artifactId__/service/impl/UserServiceImpl.java
@@ -7,20 +7,34 @@ import lombok.RequiredArgsConstructor;
 import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.ReactiveSecurityContextHolder;
+import org.springframework.security.core.context.SecurityContext;
 import org.springframework.security.oauth2.jwt.Jwt;
 import org.springframework.stereotype.Service;
+import reactor.core.publisher.Mono;
+import ${package}.${artifactId}.error.UserAuthenticationException;
 import ${package}.${artifactId}.model.UserCredentials;
 import ${package}.${artifactId}.service.UserService;
-import ${package}.${artifactId}.error.UserAuthenticationException;
 
 @Slf4j
 @Service
 @RequiredArgsConstructor
 public class UserServiceImpl implements UserService {
 
+    @Override
+    public Mono<UserCredentials> getCredentials() {
+        log.debug("subscription based getCredentials method started");
+
+        return ReactiveSecurityContextHolder
+                .getContext()
+                .map(SecurityContext::getAuthentication)
+                .switchIfEmpty(Mono.error(new UserAuthenticationException()))
+                .map(this::toUserCredentials);
+    }
+
     @SneakyThrows
-    public UserCredentials getUserCredentials(Authentication authentication) {
-        log.debug("getUserCredentials started");
+    private UserCredentials toUserCredentials(Authentication authentication) {
+        log.debug("toUserCredentials started");
 
         if (authentication != null && authentication.getPrincipal() instanceof Jwt) {
             log.trace("oauth principle type : {}", authentication.getPrincipal().getClass());
